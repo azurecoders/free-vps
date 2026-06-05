7be49ea16838b71101141eea652e9c06cdc97d86
azurecoders <azfarchannel54321@gmail.com>
2026-06-05 20:53:52 +0500

    Adding Health Route

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get("/", (req, res) => {
   res.send("OK");
 });
 
+app.get("/", (req, res) => {
+  res.json({
+    status: 200,
+    message: "OK",
+  });
+});
+
 app.listen(5000, () => {
   console.log("Server is running on PORT 5000");
 });
